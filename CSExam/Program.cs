f1b37cba7e62f22661e90565a956a2ac300a6762
annaguerrra <annaguerracastro@gmail.com>
2025-09-10 16:35:22 -0300

    configuraçoes do program.cs

--- CSExam/Program.cs
+++ CSExam/Program.cs
@@ -1,10 +1,14 @@
+using System.Text;
+using CSExam.Endpoints;
 using CSExam.Entites;
 using CSExam.Features.CreateTrip;
 using CSExam.Features.EditTrip;
 using CSExam.Features.Login;
 using CSExam.Features.ShowTrip;
 using CSExam.Services.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +24,31 @@ builder.Services.AddScoped<LoginUseCase>();
 builder.Services.AddScoped<EditTripUseCase>();
 builder.Services.AddScoped<ShowTripUseCase>();
 
+var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
+var keyBytes = Encoding.UTF8.GetBytes(jwtSecret);
+var key = new SymmetricSecurityKey(keyBytes);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidIssuer = "Turism",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = key,
+        };
+    });
+
 var app = builder.Build();
+
+app.ConfigureTripEndpoints();
+app.ConfigureUserEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
